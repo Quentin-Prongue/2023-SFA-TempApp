ab792ccfe35a3c956cd709ff325164cf65008695
Quentin Prongué <quentin.prongue@divtec.ch>
2023-06-19 09:17:00 +0200

    Changement de l'url de l'api http à https

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: 'http://tempapi.divtec.me/api',
+  baseURL: 'https://tempapi.divtec.me/api',
   timeout: 3000,
   headers: {
     Accept: 'application/json',
